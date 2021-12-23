f14212b9e09307b61c8c56329f9b0588fb039d58
Bob Belderbos <bobbelderbos@gmail.com>
2021-12-23 10:50:59 +0100

    resolve alembic issue

--- migrations/env.py
+++ migrations/env.py
@@ -1,16 +1,17 @@
 from logging.config import fileConfig
 
-from decouple import config as _config
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 from sqlmodel import SQLModel
 
+from tips.config import DATABASE_URL
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', _config("DATABASE_URL"))
+config.set_main_option('sqlalchemy.url', DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
